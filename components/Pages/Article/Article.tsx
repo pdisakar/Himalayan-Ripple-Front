052d54b84c25b2de5213cdff7f2b2013da0024f1
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-12-09 13:33:11 +0545

    working on article

--- components/Pages/Article/Article.tsx
+++ components/Pages/Article/Article.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
+import BreadCrumb from '@/components/BreadCrumb/BreadCrumb';
 
 interface ArticleProps {
     content: any;
 }
 
 export const Article: React.FC<ArticleProps> = ({ content }) => {
+    console.log("article ", content);
+
     return (
-        <div className="container mx-auto px-4 py-8 max-w-4xl">
-            <h1 className="text-4xl font-bold mb-4">{content.title}</h1>
-            {content.bannerImage && (
-                <img
-                    src={content.bannerImage}
-                    alt={content.bannerImageAlt}
-                    className="w-full h-64 object-cover rounded-lg mb-8"
+
+        <main className="testimonials-list-page container">
+            <div className='page-common-box'>
+                <div className="page-title">
+                    <div className="breadcrumb mb-1">
+                        <BreadCrumb data={content.breadcrumbs} />
+                    </div>
+                    <h1>{content.title}</h1>
+                </div>
+                <div
+                    className="prose max-w-none nt-6"
+                    dangerouslySetInnerHTML={{ __html: content.description }}
                 />
-            )}
-            <div
-                className="prose max-w-none"
-                dangerouslySetInnerHTML={{ __html: content.description }}
-            />
-        </div>
+                {/* if it got childrent then map in ul li h3 */}
+            </div>
+        </main>
+
     );
 };

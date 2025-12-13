d51b01562c663fe0b43838e65b199034f3633c82
pdisakar <barcaboysakar@gmail.com>
2025-12-13 21:18:31 +0545

    working on base url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
-const rawApiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
-export const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+const rawApiUrl = process.env.NEXT_PUBLIC_API_URL || "https://himalayan-ripple-server.onrender.com";
+export const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://himalayan-ripple-front.vercel.app/";
 export const API_URL = rawApiUrl.endsWith('/api') ? rawApiUrl : `${rawApiUrl}/api`;
 export const SERVER_URL = rawApiUrl.replace(/\/api$/, '');
 export const IMAGE_URL = SERVER_URL; // Alias for backward compatibility if needed
